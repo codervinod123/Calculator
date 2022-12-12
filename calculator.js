3b658a4b4b0c5b7a60e5bc6fafb3ee8d13ba843b
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2022-12-13 00:13:12 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+const solve=(exp)=>{
+     return eval(exp);
+ }
+ 
+
+const handleClick=(val)=>{
+   
+   if(val=='=')
+   {
+       const exp=document.getElementById("op").value;
+       const ans=solve(exp);
+       document.getElementById("op").value=ans;
+   }
+   else if(val=='C')
+   {
+      document.getElementById("op").value="";
+   }
+   else
+   {
+     document.getElementById("op").value+=val;
+   }
+}
+
